5a13eef0c1f80d0cf3bd63b9682c6ad774d46620
Tridius1 <tristan.t73@gmail.com>
2018-12-16 22:54:16 -0700

    Added Bayesian analysis

--- getSentiment.py
+++ getSentiment.py
@@ -4,6 +4,7 @@ import csv
 
 import numpy as np
 from textblob import TextBlob
+from textblob.sentiments import NaiveBayesAnalyzer
 from scipy import stats
 
 
@@ -46,7 +47,13 @@ def sentimentAnalysis(Tswitch):
 
 	for tweet in tweetList:
 
-		tbSent = TextBlob(tweet).sentiment
+		tbSent = None
+		if implimentation == 1:
+			tbSent = TextBlob(tweet, analyzer=PatternAnalyzer()).sentiment
+		elif implimentation = 2:
+			tbSent = TextBlob(tweet, analyzer=NaiveBayesAnalyzer()).sentiment
+
+
 		product = tbSent.polarity * tbSent.subjectivity
 		if product != 0:
 			products.append(product)
@@ -87,8 +94,18 @@ work_stats = None
 school_stats = None
 
 if ans[0].lower() == 'y':
-	work_stats = statisticalAnalysis('work', sentimentAnalysis('work'))
-	school_stats = statisticalAnalysis('school', sentimentAnalysis('school'))
+
+	print("Analyze with:")
+	print("    1: PatternAnalyzer (based on pattern by CLiPS)")
+	print("    2: NaiveBayesAnalyzer (using NLTK)")
+	implimentation = input()
+
+	if not implimentation == 1 or implimentation == 2:
+		print("Invalid selection")
+		exit()
+
+	work_stats = statisticalAnalysis('work', sentimentAnalysis('work', implimentation))
+	school_stats = statisticalAnalysis('school', sentimentAnalysis('school', implimentation))
 elif ans[0].lower() == 'n':
 	try:
 		with open('work_tweets_sentiments.csv', 'rb') as f:
